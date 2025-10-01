9efb73d0efbf59d6e56f525efb4ce3d7db0bf2cc
JoernStoehler <124216958+JoernStoehler@users.noreply.github.com>
2025-10-02 01:23:15 +0200

    Refine jaxtyping annotations for EHZ helper

--- src/viterbo/__init__.py
+++ src/viterbo/__init__.py
@@ -3,6 +3,12 @@
 from __future__ import annotations
 
 from .core import normalize_vector
+from .ehz import compute_ehz_capacity, standard_symplectic_matrix
 from .hello import hello_numpy
 
-__all__ = ["hello_numpy", "normalize_vector"]
+__all__ = [
+    "compute_ehz_capacity",
+    "hello_numpy",
+    "normalize_vector",
+    "standard_symplectic_matrix",
+]
